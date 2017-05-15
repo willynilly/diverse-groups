c3f37a6295e6f95bb2f53375afbab84f2acb1aba
Will Riley <wanderingwill@gmail.com>
2017-05-15 00:17:21 -0400

    Exported classes in index.js

---
+++ example.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+const EvolutionManager = require('./evolution-manager');
+const Group = require('./group');
+
+let communityCount = 100;
+let individualCount = 50;
+let groupConfigs = [{
+	name: 'dorm1',
+	minSize: 10,
+	maxSize: 15
+}, {
+	name: 'dorm2',
+	minSize: 20,
+	maxSize: 30,
+}, {
+	name: 'dorm3',
+	minSize: 15,
+	maxSize: 20,
+}];
+let featureCount = 2;
+let minFeatureValue = 0;
+let maxFeatureValue = 5;
+
+let em = new EvolutionManager();
+let individuals = em.createRandomIndividuals(individualCount, featureCount, minFeatureValue, maxFeatureValue);
+let groups = _.map(groupConfigs, (groupConfig) => {
+	let group = new Group(groupConfig.name, featureCount, groupConfig.minSize, groupConfig.maxSize);
+	return group;
+})
+let leftOverGroup = em.randomlyAssignIndividualsToGroups(individuals, groups);
+groups.push(leftOverGroup);
+
+console.log('groups', groups)
+
+let communities = em.createRandomCommunities(communityCount, groups);
+let maxCommunityCount = communities.length;
+em.setPopulation(communities, maxCommunityCount);
+em.evolve(200, true);
--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const _ = require('lodash');
 const EvolutionManager = require('./evolution-manager');
+const IndividualFactory = require('./individual-factory');
+const Individual = require('./individual');
 const Group = require('./group');
+const Community = require('./community');
 
-let communityCount = 100;
-let individualCount = 50;
-let groupConfigs = [{
-	name: 'dorm1',
-	minSize: 10,
-	maxSize: 15
-}, {
-	name: 'dorm2',
-	minSize: 20,
-	maxSize: 30,
-}, {
-	name: 'dorm3',
-	minSize: 15,
-	maxSize: 20,
-}];
-let featureCount = 2;
-let minFeatureValue = 0;
-let maxFeatureValue = 5;
-
-let em = new EvolutionManager();
-let individuals = em.createRandomIndividuals(individualCount, featureCount, minFeatureValue, maxFeatureValue);
-let groups = _.map(groupConfigs, (groupConfig) => {
-	let group = new Group(groupConfig.name, featureCount, groupConfig.minSize, groupConfig.maxSize);
-	return group;
-})
-let leftOverGroup = em.randomlyAssignIndividualsToGroups(individuals, groups);
-groups.push(leftOverGroup);
-
-console.log('groups', groups)
-
-let communities = em.createRandomCommunities(communityCount, groups);
-let maxCommunityCount = communities.length;
-em.setPopulation(communities, maxCommunityCount);
-em.evolve(200, true);
+module.exports = {
+  Individual: Individual,
+  EvolutionManager: EvolutionManager,
+  Group: Group,
+  Community: Community
+}
